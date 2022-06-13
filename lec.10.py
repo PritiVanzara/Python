a066976c3f95f40301747224e43b5b02a6e3974b
Priti Vanzara <84765222+PritiVanzara@users.noreply.github.com>
2022-06-13 13:35:44 +0530

    Add files via upload

---
+++ lec.10.py
@@ -0,0 +1,36 @@
+Python 3.10.4 (tags/v3.10.4:9d38120, Mar 23 2022, 23:13:41) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+2+3
+5
+5-2
+3
+100-20
+80
+100+20-80
+40
+38*2
+76
+99/9
+11.0
+2/4
+0.5
+10/5
+2.0
+1/2
+0.5
+23/2
+11.5
+type(11.5)
+<class 'float'>
+type(2.0)
+<class 'float'>
+type(2)
+<class 'int'>
+5%3
+2
+10%2
+0
+11%2
+1
+7.5%5
+2.5
---
+++ math_module.py
@@ -0,0 +1,53 @@
+Python 3.10.4 (tags/v3.10.4:9d38120, Mar 23 2022, 23:13:41) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+round(2.1)
+2
+round(1.5)
+2
+import math
+round(1.5)
+2
+math.floor(1.5)
+1
+math.ceil(2.1)
+3
+math.pi
+3.141592653589793
+math.e
+2.718281828459045
+2.718281828459045math
+SyntaxError: invalid decimal literal
+Syntax2.718281828459045math
+SyntaxError: invalid decimal literal
+math.sin(math.pi / 2)
+1.0
+math.sin(math.pi)
+1.2246467991473532e-16
+math.floor(math.sin(math.pi))
+0
+math.cos(0)
+1.0
+math.asin(0)
+0.0
+math.acos(0)
+1.5707963267948966
+math.hypot(3,4)
+5.0
+math.pow(2,3)
+8.0
+2 ** 3
+8
+math.pow(9,2)
+81.0
+9 ** 2
+81
+math.exp(2)
+7.38905609893065
+math.log(math.e)
+1.0
+math.log10(10000)
+4.0
+math.log2(8)
+3.0
+math.log10(1000)
+3.0
